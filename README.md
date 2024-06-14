c91b4203b217392c8f5e1df2cf50b439ce7d7a19
Dino Lozina <Dino.Lozina@live.com>
2024-06-14 16:27:14 +0200

    added a simple log

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Simple Python Test Server
+
+Just a simple python server that listens on port 8000 and returns a simple message.
+This is a fast way to test if a fluentbit is sending logs to a server.
+
+https://docs.fluentbit.io/manual/pipeline/outputs/http
+
+## How to build image
+
+```bash
+docker build -t python_server . 
+```
+
+## How to run image
+```bash
+docker run -p 8000:8000 -d python_server
+```
--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
+        logging.info("Received POST data: %s", post_data.decode('utf-8'))
         print("Received POST data:", post_data.decode('utf-8'))
         self.send_response(200)
         self.end_headers()
@@ -12,9 +14,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    print('Starting server...')
+    logging.info('Starting test server...')
+    print('Starting test server...')
     httpd.serve_forever()
 
 
